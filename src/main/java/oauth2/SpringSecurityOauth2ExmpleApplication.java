a85f80aac175a86861d37db24a65f359ae0aca2d
Lukasz Dusza <lukaszdusza280@gmail.com>
2019-02-25 18:18:42 +0100

    initial commit - start build app

---
+++ src/main/java/oauth2/SpringSecurityOauth2ExmpleApplication.java
@@ -0,0 +1,13 @@
+package oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauth2ExmpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauth2ExmpleApplication.class, args);
+    }
+
+}
